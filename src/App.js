dd6b39346725a973be6c2baabae48f0467d69e44
AleeWeb <aalexanderpr@gmail.com>
2019-01-21 12:45:57 -0500

    Finish map function and props code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Ninjas } from  './Ninjas';
 
 class App extends Component {
   state = {
-    ninjas: [
+    ninjas: [   // This state is an Array that contains different objects { }
       {name:'Ryu', age: 30, belt: 'black', id: 1},
       {name:'Yoshi', age: 20, belt: 'green', id: 2},
       {name:'Crysyal', age: 25, belt: 'pink', id: 3}
@@ -15,7 +15,7 @@ class App extends Component {
           <h1>Main</h1>
 
           <p>Welcome :)</p>
-
+            {/* Pass the State Array object "ninjas" as a prop (ninjas=) to the Ninjas component. */}
         <Ninjas ninjas={this.state.ninjas} />
 
         </div>
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -3,17 +3,25 @@ import React, { Component } from 'react';
 export class Ninjas extends React.Component {
     render(){
        // Destructuring Props
-        const { ninjas } = this.props;
+        const { ninjas } = this.props; // This is the SAME as const ninjas = this.props.ninjas;
 
-        const ninjalist = ninjas.map(ninja => {
+        // The Map function will return these HTML elements for each seperate object
+            //    in the ninjas state array.  
+
+        const ninjaList = ninjas.map(ninja => {
             return (
-                <div className="ninja">
-                    <div>Name: { name }</div>
-                    <div>Age: { age }</div>
-                    <div>Belt: { belt }</div> 
+                <div key={ninja.id} className="ninja-row"> {/* Avoid error for key */}
+                    <div>Name: { ninja.name }</div>  {/* We're cycling through ninja for each prop for the new array */}
+                    <div>Age: { ninja.age }</div>
+                    <div>Belt: { ninja.belt }</div> 
                 </div>
             )
         })
 
+        return(
+            <div className="ninja-list">
+                { ninjaList }
+            </div>
+        )
     }
 }
