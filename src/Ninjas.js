e5965661db63ecd1e69d4862ceac5749e71c5b28
AleeWeb <aalexanderpr@gmail.com>
2019-01-28 14:10:03 -0500

    The ternary operator version for conditional output

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -4,17 +4,13 @@ import React from 'react';
 const Ninjas = ({ ninjas }) => { 
 
     const ninjaList = ninjas.map(ninja => {
-        if (ninja.age > 20) {
-            return (
+            return ninja.age > 20 ? (
                 <div key={ninja.id} className="ninja-row">
                     <div>Name: {ninja.name}</div>
                     <div>Age: {ninja.age}</div>
                     <div>Belt: {ninja.belt}</div>
                 </div>
-            )
-        } else {
-            return null
-        }
+            ) :null;
     })
 
     return (
