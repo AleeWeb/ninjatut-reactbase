302a24fe43c2d264ce8c20f13fcac0866a889c38
AleeWeb <aalexanderpr@gmail.com>
2019-01-28 14:05:46 -0500

    Conditional output using the if statement

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
 
-//Turn this into a Functional Component because the State is in the Class Based App.js Root Component
-// This functional component is only concerned with UI and how data displays.
-
-// Pass the props as a paramenter in Functional components. Class Based components use this.props.
-
-const Ninjas = ({ninjas}) => { // Destructure props in parameter. Removes const { ninjas} = props;
+const Ninjas = ({ ninjas }) => { 
 
     const ninjaList = ninjas.map(ninja => {
-        return (
-            <div key={ninja.id} className="ninja-row"> 
-                <div>Name: {ninja.name}</div> 
-                <div>Age: {ninja.age}</div>
-                <div>Belt: {ninja.belt}</div>
-            </div>
-        )
+        if (ninja.age > 20) {
+            return (
+                <div key={ninja.id} className="ninja-row">
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div>
+                </div>
+            )
+        } else {
+            return null
+        }
     })
 
     return (
