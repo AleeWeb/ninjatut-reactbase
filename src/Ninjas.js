839db3d603676a4033a5d75b5af2716116805b30
AleeWeb <aalexanderpr@gmail.com>
2019-01-28 13:17:49 -0500

    Save new notes just for record

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const Ninjas = ({ninjas}) => {
+
+//Turn this into a Functional Component because the State is in the Class Based App.js Root Component
+// This functional component is only concerned with UI and how data displays.
+
+// Pass the props as a paramenter in Functional components. Class Based components use this.props.
+
+const Ninjas = ({ninjas}) => { // Destructure props in parameter. Removes const { ninjas} = props;
 
     const ninjaList = ninjas.map(ninja => {
         return (
