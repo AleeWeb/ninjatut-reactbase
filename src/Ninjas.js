7e59da2fe512fa2be613e0aa674c4d2ad2b5b09e
AleeWeb <aalexanderpr@gmail.com>
2019-01-28 01:31:42 -0500

    Step to turn child component into a functional based UI components because it has the props

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class Ninjas extends React.Component {
-    render(){
-       // Destructuring Props
-        const { ninjas } = this.props; // This is the SAME as const ninjas = this.props.ninjas;
+const Ninjas = ({ninjas}) => {
 
-        // The Map function will return these HTML elements for each seperate object
-            //    in the ninjas state array.  
-
-        const ninjaList = ninjas.map(ninja => {
-            return (
-                <div key={ninja.id} className="ninja-row"> {/* Avoid error for key */}
-                    <div>Name: { ninja.name }</div>  {/* We're cycling through ninja for each prop for the new array */}
-                    <div>Age: { ninja.age }</div>
-                    <div>Belt: { ninja.belt }</div> 
-                </div>
-            )
-        })
-
-        return(
-            <div className="ninja-list">
-                { ninjaList }
+    const ninjaList = ninjas.map(ninja => {
+        return (
+            <div key={ninja.id} className="ninja-row"> 
+                <div>Name: {ninja.name}</div> 
+                <div>Age: {ninja.age}</div>
+                <div>Belt: {ninja.belt}</div>
             </div>
         )
-    }
+    })
+
+    return (
+        <div className="ninja-list">
+            {ninjaList}
+        </div>
+    )
+
 }
+
+export default Ninjas
