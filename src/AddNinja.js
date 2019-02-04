cceeb20b8c5c9dd295dc607e97504e180f58fce1
AleeWeb <aalexanderpr@gmail.com>
2019-02-03 22:51:44 -0500

    Added a form to the new class based component

--- src/AddNinja.js
+++ src/AddNinja.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
 
 class AddNinja extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            data: [
-
-            ]
-        }
+    state= {
+        name: null,
+        age: null,
+        belt: null
     }
 
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value         // e is the event object in parameter above. 
+        })                                     // target is the onChange={} function below     
+    }                                         // value is the text in between labels and id are the ids duh!
+
+    handleSubmit = (e) => {
+        e.preventDefault();   // Prevents form refresh on the page.
+       // console.log(this.state);
+    }
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}> {/* Use onSubmit to capture both the click event on the button and the user presses enter on the form.  */}
                     <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" onChange={} />
+                    <input type="text" id="name" onChange={this.handleChange} />
 
                     <label htmlFor="name">Age:</label>
-                    <input type="text" id="age" onChange={} />
+                    <input type="text" id="age" onChange={this.handleChange} />
 
                     <label htmlFor="name">Belt:</label>
-                    <input type="text" id="belt" onChange={} />
+                    <input type="text" id="belt" onChange={this.handleChange} />
 
                     <button>Submit</button>
                 </form>
@@ -33,3 +39,4 @@ class AddNinja extends React.Component {
     }
 }
 
+export default AddNinja;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
   state = {
     ninjas: [   // This state is an Array that contains different objects { }
-      {name:'Ryu', age: 30, belt: 'black', id: 1},
-      {name:'Yoshi', age: 20, belt: 'green', id: 2},
-      {name:'Crysyal', age: 25, belt: 'pink', id: 3}
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crysyal', age: 25, belt: 'pink', id: 3 }
     ]
   }
-    render() {
-      return (
-        <div>
-          <h1>Main</h1>
+  render() {
+    return (
+      <div>
+        <h1>Main</h1>
 
-          <p>Welcome :)</p>
-            {/* Pass the State Array object "ninjas" as a prop (ninjas=) to the Ninjas component. */}
+        <p>Welcome :)</p>
+        {/* Pass the State Array object "ninjas" as a prop (ninjas=) to the Ninjas component. */}
         <Ninjas ninjas={this.state.ninjas} />
 
-        </div>
-      );
-    }
+        <AddNinja />
+
+      </div>
+    );
+  }
 }
 
 export default App;
